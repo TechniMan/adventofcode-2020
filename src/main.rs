e1f1501505619fbbb97e7c357a2804bd58e05f0d
Will Thomas <technimanx@googlemail.com>
2020-12-01 23:40:49 +0000

    Completed Day 1 Parts 1 and 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = std::fs::read_to_string("src/input.txt")?;
+    let part = 2;
+
+    let list : Vec<&str> = input.split('\n').collect();
+    let numbers : Vec<i32> = list.iter().map(|&n| n.parse::<i32>().unwrap()).collect();
+
+    if part == 1 {
+        for i in &numbers {
+            for j in &numbers {
+                if i + j == 2020 {
+                    println!("answer:{}", i * j);
+                    return Ok(());
+                }
+            }
+        }
+    }
+    else if part == 2 {
+        for i in &numbers {
+            for j in &numbers {
+                for k in &numbers {
+                    if i + j + k == 2020 {
+                        println!("answer:{}", i * j * k);
+                        return Ok(());
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
