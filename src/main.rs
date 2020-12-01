76130f73d3b5cba9cc2203398f5ff0d0ebfecd14
Will Thomas <technimanx@googlemail.com>
2020-12-01 23:45:06 +0000

    Added comments to day 1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input = std::fs::read_to_string("src/input.txt")?;
+    // use puzzle part 2 instead of puzzle part 1
     let part = 2;
 
+    // read the file
+    let input = std::fs::read_to_string("src/input.txt")?;
+
+    // split file input from string to list
     let list : Vec<&str> = input.split('\n').collect();
+    // convert list of strings to integers
     let numbers : Vec<i32> = list.iter().map(|&n| n.parse::<i32>().unwrap()).collect();
 
     if part == 1 {
